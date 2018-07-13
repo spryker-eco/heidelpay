47b9b1c0837f6b6eeb9b6d038adf537dd9353aca
Ivan Pugach <ivan.pugach@spryker.com>
2018-07-13 13:56:59 +0000

    Add new payment method to yves to checkout form

---
+++ src/SprykerEco/Yves/Heidelpay/Controller/EasyCreditController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * MIT License
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerEco\Yves\Heidelpay\Controller;
+
+use Generated\Shared\Transfer\HeidelpayEasyCreditAuthorizeFormTransfer;
+use Generated\Shared\Transfer\HeidelpayTransactionLogTransfer;
+use Spryker\Yves\Kernel\Controller\AbstractController;
+
+/**
+ * @method \SprykerEco\Yves\Heidelpay\HeidelpayFactory getFactory()
+ * @method \SprykerEco\Client\Heidelpay\HeidelpayClientInterface getClient()
+ */
+class EasyCreditController extends AbstractController
+{
+}
