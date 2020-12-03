e56e2d2647958dc715c6741559edf8106cced70b
Roman Havrylko <roman.havrylko@spryker.com>
2020-12-03 09:36:03 +0200

    SUPESC-213 Use facade in old plugin

--- src/SprykerEco/Zed/Heidelpay/Communication/Plugin/Checkout/Oms/Condition/IsCaptureApprovedPlugin.php
+++ src/SprykerEco/Zed/Heidelpay/Communication/Plugin/Checkout/Oms/Condition/IsCaptureApprovedPlugin.php
@@ -7,15 +7,11 @@
 
 namespace SprykerEco\Zed\Heidelpay\Communication\Plugin\Checkout\Oms\Condition;
 
-use Orm\Zed\Heidelpay\Persistence\SpyPaymentHeidelpayTransactionLog;
 use Orm\Zed\Sales\Persistence\SpySalesOrderItem;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 use Spryker\Zed\Oms\Dependency\Plugin\Condition\ConditionInterface;
-use SprykerEco\Shared\Heidelpay\HeidelpayConfig;
 
 /**
- * @deprecated Use {@link \SprykerEco\Zed\Heidelpay\Communication\Plugin\Checkout\Oms\Condition\IsCaptureSuccessPlugin} instead.
- *
  * @method \SprykerEco\Zed\Heidelpay\Business\HeidelpayFacadeInterface getFacade()
  * @method \SprykerEco\Zed\Heidelpay\Persistence\HeidelpayQueryContainerInterface getQueryContainer()
  * @method \SprykerEco\Zed\Heidelpay\Communication\HeidelpayCommunicationFactory getFactory()
@@ -35,43 +31,7 @@ class IsCaptureApprovedPlugin extends AbstractPlugin implements ConditionInterfa
      */
     public function check(SpySalesOrderItem $orderItem): bool
     {
-        return $this->isCaptureSuccessful($orderItem->getFkSalesOrder());
-    }
-
-    /**
-     * @param int $idSalesOrder
-     *
-     * @return bool
-     */
-    protected function isCaptureSuccessful(int $idSalesOrder): bool
-    {
-        $captureTransactionLog = $this->getCaptureTransactionLogEntry($idSalesOrder);
-        if ($captureTransactionLog === null) {
-            return false;
-        }
-
-        return $this->isTransactionSuccessful($captureTransactionLog);
-    }
-
-    /**
-     * @param int $idSalesOrder
-     *
-     * @return \Orm\Zed\Heidelpay\Persistence\SpyPaymentHeidelpayTransactionLog|null
-     */
-    protected function getCaptureTransactionLogEntry(int $idSalesOrder): ?SpyPaymentHeidelpayTransactionLog
-    {
-        $transactionLogQuery = $this->getQueryContainer()->queryCaptureTransactionLog($idSalesOrder);
-
-        return $transactionLogQuery->findOne();
-    }
-
-    /**
-     * @param \Orm\Zed\Heidelpay\Persistence\SpyPaymentHeidelpayTransactionLog $caprureTransactionLog
-     *
-     * @return bool
-     */
-    protected function isTransactionSuccessful(SpyPaymentHeidelpayTransactionLog $caprureTransactionLog): bool
-    {
-        return $caprureTransactionLog->getResponseCode() === HeidelpayConfig::CAPTURE_TRANSACTION_STATUS_OK;
+        return $this->getFacade()
+            ->isCaptureSuccessful($orderItem);
     }
 }
