e1b570b10f4a878788805554a8c6679337118363
Anton Zubariev <anton.zubariev@spryker.com>
2021-01-19 17:26:22 +0200

    [SUPESC-213] Travis fix: set empty dependency for Sales provider.

--- tests/SprykerEcoTest/Zed/Heidelpay/Business/DataProviders/Payment/PaymentHeidelpayTransferBuilderTrait.php
+++ tests/SprykerEcoTest/Zed/Heidelpay/Business/DataProviders/Payment/PaymentHeidelpayTransferBuilderTrait.php
@@ -10,6 +10,7 @@ namespace SprykerEcoTest\Zed\Heidelpay\Business\DataProviders\Payment;
 use Generated\Shared\Transfer\CustomerTransfer;
 use Generated\Shared\Transfer\OrderTransfer;
 use Orm\Zed\Sales\Persistence\SpySalesOrder;
+use Spryker\Zed\Sales\SalesDependencyProvider;
 use SprykerEco\Zed\Heidelpay\Business\HeidelpayFacadeInterface;
 
 trait PaymentHeidelpayTransferBuilderTrait
@@ -22,6 +23,11 @@ trait PaymentHeidelpayTransferBuilderTrait
      */
     protected function getOrderTransfer(HeidelpayFacadeInterface $heidelpayFacade, SpySalesOrder $salesOrder): OrderTransfer
     {
+        $this->tester->setDependency(
+            SalesDependencyProvider::PLUGINS_ORDER_ITEM_EXPANDER,
+            []
+        );
+
         $paymentTransfer = $heidelpayFacade->getPaymentByIdSalesOrder($salesOrder->getIdSalesOrder());
         $orderTransfer = $this->heidelpayToSales->getOrderByIdSalesOrder($salesOrder->getIdSalesOrder());
         $orderTransfer->setHeidelpayPayment($paymentTransfer);
